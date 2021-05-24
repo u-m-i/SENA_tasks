e22ebab9f04245e73f73d5af15e274e380798ada
umi <asiessoyeltier1@gmail.com>
2021-05-24 15:36:01 -0500

    The b point done

---
+++ taller_2_estructuras/act_p_b.py
@@ -0,0 +1,29 @@
+# Function to determinate the player status
+
+def liga(age,accurate):
+
+    if age >= 18 and age <= 22 and accurate >= 80:
+        return f"Bro felicitaciones puedes pertenecer a: LIGA PROFESIONAL DE FUTBOL"
+    elif age >= 18 and age <= 22 and acurate < 80:
+        return f"Bro felicitaciones puedes pertencer a: LIGA SEMIPROFESIONAL"
+    else:
+        return False
+
+
+if __name__ == '__main__':
+
+    # Ask to user
+
+    user_age = int(input("Porfavor ingrese la edad del jugador entre los 18-22 años: "))
+    user_accurate = int(input("Ahora ingrese el puntaje del jugador "))
+
+    # Invocate the function into a variable
+
+    j = liga(user_age, user_accurate)
+
+    if j:
+        print(j)
+    else:
+        print("Por el momento no puedes aplicar a ningún nivel semi o profesinal")
+    
+
