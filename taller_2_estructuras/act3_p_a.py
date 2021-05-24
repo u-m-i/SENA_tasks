67e1ba5d80e07d9eace226cbe86fe1136cb75163
umi <asiessoyeltier1@gmail.com>
2021-05-24 16:05:09 -0500

    The third activitie point a done

---
+++ taller_2_estructuras/act3_p_a.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    my_list = [ i for i in range(2000,8000) if i % 55 == 0]
+    print(f"""La lista de número entre el 2000 y el 8000 que son múltiplos de 11 y 55 son 
+    {my_list}""")
