d3ad5312a02e8cb390006f48ecaa00cc281775fa
umi <asiessoyeltier1@gmail.com>
2021-05-26 10:29:07 -0500

    Last Exersice DONE > !! :

--- taller_2_estructuras/act3_p_b.py
+++ taller_2_estructuras/act3_p_b.py
@@ -1,9 +1,21 @@
 if __name__ == '__main__':
     i = 9
+    j = 1
+    k = 4
     while i != 0:
-        j = abs((i*(5-(i-4)))/9)
-        print(f"#"*j)
+        while j != 6:
+            print("* "*j)
+            j +=1
+        while k != 0 :
+            print("* "*k)
+            k -=1
         i -= 1
+
+
+
+        #j = abs((i*(5-(i-4)))/9)
+        #print(f"#"*j)
+        #i -= 1
 
     print("#"*60)
     print("""Recuerde ver este trabajo completo en:
